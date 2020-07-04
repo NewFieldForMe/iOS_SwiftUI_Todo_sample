c0668e6aee85a5101c099754752a2e48f4000984
RyoYamada <newfield1001@gmail.com>
2020-07-04 16:46:15 +0900

    Show test data list

--- NGenTodo/ContentView.swift
+++ NGenTodo/ContentView.swift
@@ -9,8 +9,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    var taskItems = testData
+
     var body: some View {
-        Text("Hello, World!")
+        List(taskItems) { item in
+            Text(item.title)
+        }
     }
 }
 
