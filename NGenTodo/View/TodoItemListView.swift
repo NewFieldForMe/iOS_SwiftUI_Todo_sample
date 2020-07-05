7c12a663011dcf24de112f04c4a3305dfda19a86
RyoYamada <newfield1001@gmail.com>
2020-07-05 12:08:29 +0900

    Add floating button

--- NGenTodo/View/TodoItemListView.swift
+++ NGenTodo/View/TodoItemListView.swift
@@ -12,14 +12,40 @@ struct TodoItemListView: View {
     var taskItems = testData
 
     var body: some View {
-        List {
-            ForEach(TodoItem.TodoState.allCases) { state in
-                Section(header: Text(state.label)) {
-                    ForEach(self.taskItems.items(state: state)) { item in
-                        Text(item.title)
+        ZStack {
+            List {
+                ForEach(TodoItem.TodoState.allCases) { state in
+                    Section(header: Text(state.label)) {
+                        ForEach(self.taskItems.items(state: state)) { item in
+                            Text(item.title)
+                        }
                     }
                 }
             }
+
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        // TODO: Add Action
+                    }, label: {
+                        Text("+")
+                            .font(.system(.largeTitle))
+                            .frame(width: 77, height: 70)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 7)
+                    })
+                    .background(Color.blue)
+                    .cornerRadius(38.5)
+                    .padding()
+                    .shadow(color: Color.black.opacity(0.3),
+                            radius: 3,
+                            x: 3,
+                            y: 3)
+                }
+            }
+
         }
     }
 }
