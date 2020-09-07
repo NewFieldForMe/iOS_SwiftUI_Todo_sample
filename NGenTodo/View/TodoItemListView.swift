18af3742f464c86a31baaa01a2d5882cc23768d7
RyoYamada <newfield1001@gmail.com>
2020-09-07 18:09:44 +0900

    separate ready and done list

--- NGenTodo/View/TodoItemListView.swift
+++ NGenTodo/View/TodoItemListView.swift
@@ -20,20 +20,39 @@ struct TodoItemListView: View {
             NavigationView {
                 ZStack {
                     List {
-                        ForEach(vm.todos) { item in
-                            NavigationLink(destination: InputTodoView(item)) {
-                                TodoItemListCellView(todo: item)
-                                    .contextMenu {
-                                        Button(action: {
-                                            self.vm.delete(item)
-                                        }, label: {
-                                            Text("Delete Todo")
-                                        })
+                        Section(header: Text("Ready")) {
+                            ForEach(vm.todos.filter { $0.todoState == .ready }) { item in
+                                NavigationLink(destination: InputTodoView(item)) {
+                                    TodoItemListCellView(todo: item)
+                                        .contextMenu {
+                                            Button(action: {
+                                                self.vm.delete(item)
+                                            }, label: {
+                                                Text("Delete Todo")
+                                            })
+                                    }
                                 }
                             }
+                            .onDelete(perform: self.vm.delete)
+                            .onMove(perform: self.vm.move)
                         }
-                        .onDelete(perform: self.vm.delete)
-                        .onMove(perform: self.vm.move)
+
+                        Section(header: Text("Done")) {
+                            ForEach(vm.todos.filter { $0.todoState == .done }) { item in
+                                NavigationLink(destination: InputTodoView(item)) {
+                                    TodoItemListCellView(todo: item)
+                                        .contextMenu {
+                                            Button(action: {
+                                                self.vm.delete(item)
+                                            }, label: {
+                                                Text("Delete Todo")
+                                            })
+                                    }
+                                }
+                            }
+                            .onDelete(perform: self.vm.delete)
+                            .onMove(perform: self.vm.move)
+                        }
                     }
                     .onAppear(perform: self.vm.onAppear)
                     .navigationBarTitle("Todo List")
@@ -44,6 +63,7 @@ struct TodoItemListView: View {
                             Image(systemName: "line.horizontal.3")
                                 .imageScale(.large)
                         })), trailing: EditButton())
+
 
                     FloatingButtonView() {
                         self.vm.showingAddTodo.toggle()
