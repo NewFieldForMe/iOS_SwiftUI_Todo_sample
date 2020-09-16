c52d466748b3c76d29ffaad805d13e61507c1861
RyoYamada <newfield1001@gmail.com>
2020-09-16 11:44:21 +0900

    add StatisticViewModel

--- NGenTodo/View/StatisticView.swift
+++ NGenTodo/View/StatisticView.swift
@@ -9,6 +9,7 @@
 import SwiftUI
 
 struct StatisticView: View {
+    @ObservedObject var vm = StatisticViewModel()
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
---
+++ NGenTodo/ViewModel/StatisticViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  StatisticViewModel.swift
+//  NGenTodo
+//
+//  Created by yamada.ryo on 2020/09/16.
+//  Copyright © 2020 yamada.ryo. All rights reserved.
+//
+
+import Foundation
+
+class StatisticViewModel: ObservableObject {
+
+}
