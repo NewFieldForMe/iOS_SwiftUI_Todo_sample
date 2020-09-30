95af52b096c954272cc4f578253c08b01b5468cb
RyoYamada <newfield1001@gmail.com>
2020-09-30 17:03:08 +0900

    add TemplateListViewModel

---
+++ NGenTodo/ViewModel/TemplateListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TemplateListViewModel.swift
+//  NGenTodo
+//
+//  Created by yamada.ryo on 2020/09/30.
+//  Copyright © 2020 yamada.ryo. All rights reserved.
+//
+
+import SwiftUI
+
+class TemplateListViewModel: ObservableObject {
+    var template: [TemplateData] = []
+}
